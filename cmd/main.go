8bc7e314dba46ab044a954063978994f6448a174
Felix Dittrich <felix.dittrich@yahoo.com>
2022-04-19 16:40:11 +0200

    Initial start, destin to change dramatically

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"de.stuttgart.hft/DBS2-Backend/handler"
+	"github.com/gin-gonic/gin"
+	//"database/sql"
+	//_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	r := gin.Default()
+	r.GET("/ping", handler.PingGet())
+	r.Run(":8080")
+	//db, err := sql.Open("sqlite3", "./database.db")
 }
