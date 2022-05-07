a300ba7c49e12d2556dcff63037d72b60259e2d9
Felix Dittrich <felix.dittrich@yahoo.com>
2022-05-07 14:23:44 +0200

    fixed json validator syntax

--- pkg/models/response.go
+++ pkg/models/response.go
@@ -1,13 +1,13 @@
 package models
 
 type Message struct {
-	Code    uint32 `json: "code"`
-	Message string `json: "message"`
+	Code    uint32 `json:"code"`
+	Message string `json:"message"`
 }
 
 type Response[T any] struct {
-	Success  bool      `json: "success"`
-	Errors   []Message `json: "errors"`
-	Messages []Message `json: "messages"`
+	Success  bool      `json:"success"`
+	Errors   []Message `json:"errors"`
+	Messages []Message `json:"messages"`
 	Result   T         `json:"result"`
 }
